6c9bd60b932e70329b52829cc78375080add3ccc
Rob Thijssen <rthijssen@gmail.com>
2022-12-20 19:01:18 +0200

    use discord.js (only)

--- discord/registration/index.js
+++ discord/registration/index.js
@@ -1,23 +1,28 @@
-const fs = require('node:fs');
-const path = require('node:path');
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
+const { REST, Routes, SlashCommandBuilder } = require('discord.js');
+
 const discord = {
     clientId: process.env.DISCORD_APPLICATION_ID,
     guildId: process.env.DISCORD_GUILD_ID,
     token: process.env.DISCORD_BOT_TOKEN,
 };
-
-const commands = [];
-const commandFiles = fs.readdirSync(path.resolve(__dirname, 'commands')).filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-    const command = require(`${path.resolve(__dirname, 'commands')}/${file}`);
-    commands.push(command.data.toJSON());
-}
-
-const rest = new REST({ version: '9' }).setToken(discord.token);
-
+const commands = ['dol', 'kar', 'ksm', 'movr'].map((token) => (
+    new SlashCommandBuilder()
+        .setName(`gimme-${token}`)
+        .setDescription(`request ${token} tokens`)
+        .addStringOption((option) => (
+            option
+                .setName('address')
+                .setRequired(true)
+                .setDescription(`address to receive ${token} token`)
+        ))
+));
+const rest = new REST({ version: '10' }).setToken(discord.token);
+rest.put(Routes.applicationGuildCommands(discord.clientId, discord.guildId), { body: [] })
+    .then(() => console.log('deleted old guild commands.'))
+    .catch(console.error);
+rest.put(Routes.applicationCommands(discord.clientId), { body: [] })
+    .then(() => console.log('deleted old application commands.'))
+    .catch(console.error);
 rest.put(Routes.applicationGuildCommands(discord.clientId, discord.guildId), { body: commands })
     .then(() => console.log('Successfully registered application commands.', JSON.stringify(commands)))
     .catch(console.error);
