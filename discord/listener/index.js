dbe6685467b0e6a631f196fb7c78c0a7c8b883fa
Rob Thijssen <rthijssen@gmail.com>
2022-12-22 20:47:50 +0200

    debug tx

--- discord/listener/index.js
+++ discord/listener/index.js
@@ -3,32 +3,29 @@ const { SQS } = require('aws-sdk');
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 
 const processQueuedMessages = async (sqs, faucet) => {
-    try {
-        const queue = await sqs.receiveMessage({
-            MaxNumberOfMessages: 10,
-            MessageAttributeNames: ['All'],
-            QueueUrl: process.env.AWS_SQS_URL,
-            VisibilityTimeout: 10,
-            WaitTimeSeconds: 0
-        }).promise();
-        if (!!queue.Messages) {
-            console.log(`fetched ${queue.Messages.length} messages from the queue.`);
-            for (const message of queue.Messages) {
-                await faucet.process_transfer(JSON.parse(message.Body));
-                try {
-                    await sqs.deleteMessage({ QueueUrl: process.env.AWS_SQS_URL, ReceiptHandle: message.ReceiptHandle }).promise();
-                } catch (error) {
-                    console.error(error);
-                }
+    const queue = await sqs.receiveMessage({
+        MaxNumberOfMessages: 10,
+        MessageAttributeNames: ['All'],
+        QueueUrl: process.env.AWS_SQS_URL,
+        VisibilityTimeout: 10,
+        WaitTimeSeconds: 0
+    }).promise();
+    if (!!queue.Messages) {
+        console.log(`fetched ${queue.Messages.length} messages from the queue.`);
+        for (const message of queue.Messages) {
+            await faucet.process_transfer(JSON.parse(message.Body));
+            try {
+                const deleteMessageResponse = await sqs.deleteMessage({ QueueUrl: process.env.AWS_SQS_URL, ReceiptHandle: message.ReceiptHandle }).promise();
+                console.log(JSON.stringify({deleteMessageResponse}));
+            } catch (error) {
+                //console.error(error);
+                console.log(JSON.stringify({error}));
             }
-            console.log(`processed ${queue.Messages.length} messages from the queue.`);
-        } else {
-            console.log(`observed an empty queue.`);
-            await new Promise((r) => setTimeout(r, 3000));
         }
-    } catch (error) {
-        console.error(error);
-        await new Promise((r) => setTimeout(r, 10000));
+        console.log(`processed ${queue.Messages.length} messages from the queue.`);
+    } else {
+        console.log(`observed an empty queue.`);
+        await new Promise((r) => setTimeout(r, 3000));
     }
 };
 
@@ -39,6 +36,11 @@ const processQueuedMessages = async (sqs, faucet) => {
     await client.login(process.env.DISCORD_BOT_TOKEN);
     const faucet = new Faucet(client, process.env.DOLPHIN_FAUCET_MNEMONIC);
     while (true) {
-        await processQueuedMessages(sqs, faucet);
+        try {
+            await processQueuedMessages(sqs, faucet);
+        } catch (error) {
+            console.log(JSON.stringify({error}));
+            await new Promise((r) => setTimeout(r, 10000));
+        }
     }
 })();
